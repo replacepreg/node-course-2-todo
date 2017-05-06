d2f72947ec1252d2a7a031a2b1afef16130b3030
Mustafa <mustafa@Mustafas-MacBook-Air.local>
2017-05-06 17:39:33 +0300

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,31 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to the database');
+  }
+  console.log('Connected to database');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('590c956ad01824080a50f8ce')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({name:'Andrew Mead'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('An error occured.')
+  });
+
+  //db.close();
+});
